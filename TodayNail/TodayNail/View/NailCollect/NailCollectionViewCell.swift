5a39ebff0308995f12d9234c218a094913ad1c86
ryr0121 <ryr0121@gmail.com>
2023-11-23 10:28:13 +0900

    feat: 네일 모아보기 화면 UI 구현

---
+++ TodayNail/TodayNail/View/NailCollect/NailCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  NailCollectionViewCell.swift
+//  TodayNail
+//
+//  Created by 김초원 on 11/23/23.
+//
+
+import UIKit
+
+class NailCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
