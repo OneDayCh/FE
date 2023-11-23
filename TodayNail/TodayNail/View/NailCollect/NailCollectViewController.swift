9af6db026f55793fcc70668afdbb96808ec22000
ryr0121 <ryr0121@gmail.com>
2023-11-23 10:28:47 +0900

    feat: 네일 모아보기 목록 화면 UI 구현

--- TodayNail/TodayNail/View/NailCollect/NailCollectViewController.swift
+++ TodayNail/TodayNail/View/NailCollect/NailCollectViewController.swift
@@ -14,6 +14,7 @@ class NailCollectViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigationBar()
+        setupCollectionView()
     }
     
     func setupNavigationBar() {
@@ -40,9 +41,30 @@ class NailCollectViewController: UIViewController {
         self.navigationItem.rightBarButtonItems = [bagItem, calendarItem, searchItem]
     }
     
+    func setupCollectionView() {
+       collectionView.dataSource = self
+       collectionView.delegate = self
+       collectionView.register(UINib(nibName: "NailCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "NailCollectionViewCell")
+   }
     
     @objc private func moveToSearchVC() {
         
     }
 
+}
+
+extension NailCollectViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "NailCollectionViewCell", for: indexPath) as? NailCollectionViewCell else { return UICollectionViewCell() }
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = (UIScreen.main.bounds.size.width-30)/2
+        return CGSize(width: width, height: width*1.5)
+    }
 }
